d673426ccf257d567d3fa81be7f482b0746e3155
Francisco-dos-Santos <franciscodossantosmr@gmail.com>
2024-01-21 05:20:58 -0300

    add news conceits in class

--- classes/classes.js
+++ classes/classes.js
@@ -1,22 +1,22 @@
-console.log("testando");
-class Vieble{
+
+class Vihicle{
   constructor({id,latitude, longitude}){
     this.id=id;
-    this.time=Date.now();
     this.setPostion({latitude,longitude});
-  };
-  getPostion=function({latitude,longitude}){
-    this.time=Date.now();
-    this.latitude=latitude;
-    this.longitude=longitude
-  };
-  setPostion=function({latitude,longitude}){
+  };  
+  getPostion=function(){
     return{
       latitude:this.latitude,
       longitude:this.longitude
     }
-  }
-}
-
-let vieble1=new Vieble(id="LA045",latitude=19.43232,longitude=56.89975);
-console.log(vieble1.setPostion())
+  };
+  setPostion=function({latitude,longitude}){
+    this.time=Date.now();
+    this.latitude=latitude;
+    this.longitude=longitude;
+  };
+};
+console.log("testando");
+let vihicle1= new Vihicle( {id: "LA045", latitude: 19.43232, longitude: 56.89975});
+vihicle1.setPostion({longitude: 59.456, latitude:40.6754});
+console.log(vihicle1.getPostion())
