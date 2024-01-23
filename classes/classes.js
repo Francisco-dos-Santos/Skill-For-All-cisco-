4b70353a813da3734fe37df78ed2e1ba468e891f
Francisco-dos-Santos <franciscodossantosmr@gmail.com>
2024-01-23 11:17:06 -0300

    finalizando class

--- classes/classes.js
+++ classes/classes.js
@@ -17,6 +17,16 @@
   };
 };
 console.log("testando");
+// instancing object
 let vihicle1= new Vihicle( {id: "LA045", latitude: 19.43232, longitude: 56.89975});
 vihicle1.setPostion({longitude: 59.456, latitude:40.6754});
-console.log(vihicle1)
+console.log(vihicle1);
+
+/* 
+  instances to objects
+  for describle the exits the instance the objects using following:
+  #nameObj instanceof class -> when if khow a class especify is operator binario (return true/false)
+  #nameObject.Constructor.name-> useing for khow if o object from that instance the class
+*/
+console.log(vihicle1.constructor.name);
+console.log( vihicle1 instanceof Vihicle);   
