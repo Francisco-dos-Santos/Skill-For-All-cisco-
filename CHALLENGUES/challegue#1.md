b1b1a7304ed9e5535908d98a17c616a17a879511
Francisco-dos-Santos <franciscodossantosmr@gmail.com>
2024-02-19 19:56:19 -0300

    add new file

--- CHALLENGUES/challegue#1.md
+++ CHALLENGUES/challegue#1.md
@@ -1,4 +1,4 @@
-# Scenario
+# Scenario # 1
 Tente preparar um sistema simples para armazenar informações sobre alunos e professores e combiná-las para tutoria.
 
 Crie uma classe User para criar objetos para professores e alunos. O construtor deve pegar os dados do usuário (nome, sobrenome, email, função), mas certifique-se de criar as propriedades apropriadas.
@@ -13,3 +13,23 @@ editCourse(curso, nível) - que lhe permitirá alterar o nível associado ao cur
 sendMessage(from, message) - que permitirá que você envie uma mensagem 'message' do usuário 'de' para o usuário descrito no objeto; informações completas sobre a mensagem enviada devem ser armazenadas no cache local (dica: use uma matriz para isso); o envio da mensagem em si só será simulado, declarar previamente a função sendEmail(de, para, mensagem) {} e utilizá-la no local apropriado;
 
 showMessagesHistory() - que exibirá o histórico de todas as mensagens enviadas ao usuário no console.
+
+# Scenario # 2
+Create a new class, ExtendedUser, that will inherit from the User class.
+
+Put a setter and getter named fullName in it. The getter should return the first name and last name concatenated into one string. The setter takes the concatenated first and last name (*e.g. 'Rafael Fifer') and splits it into first and last name (the split method), changing the appropriate properties of the object.
+
+Based on the ExtendedUser class, create two more classes, Teacher and Student (inheritance). They should not have any new methods or properties, but only the default roles in their constructors to 'teacher' or 'student' respectively (i.e. their constructors will take three parameters instead of four: name, surname, and email);
+
+Test your solution using the following code:
+
+# Scenario # 3
+Modify the ExtendedUser class (rewrite it) by adding a static match method to it. The method should retrieve the teacher object, student object, and optionally a course name. Its task is to find the match between the student and the teacher.
+
+In case the course name is not provided, the method should return:
+
+an empty array if there is no match (the teacher does not teach courses the student is interested in, or teaches courses at a lower level)
+an array with {course, level} objects, if the teacher teaches the courses the student is interested in.
+If the course name is passed as the last argument, then the method should return the {course, level} object in case of a correct match, or undefined otherwise.
+
+Test your solution using the following code:
--- CHALLENGUES/challegue1.js
+++ CHALLENGUES/challegue1.js
@@ -1,84 +1,170 @@
-function sendEmail(from, to, message){}
+// CHALLEGUE#1
+function sendEmail(from, to, message) {}
 
 class User {
-  constructor({ name, surName, email, role }) {
+  constructor({ name, surname, email, role }) {
     this.name = name;
-    this.surName = surName;
+    this.surname = surname;
     this.email = email;
     this.role = role;
     this.courses = [];
     this.messages = [];
-  };
-  addCourse(course, nevel) {
+  }
+  addCourse(course, level) {
     for (let i = 0; i < this.courses.length; i++) {
-      if (this.courses[i].course === course){
+      if (this.courses[i].course === course) {
         return;
       }
     }
-    this.courses.push({ course, nevel });
-  };
+    this.courses.push({ course, level });
+  }
   removeCourse(course) {
     let courses = this.courses.filter((el) => el.course !== course);
     if (courses) {
       this.courses = courses;
     }
-  };
-  editCourse(course, nevel) {
+  }
+  editCourse(course, level) {
     for (let el of this.courses) {
-      if (el.course == course) {
-        this.nevel = nevel;
+      if (el.course === course) {
+        el.level = level;
         break;
       }
     }
-  };
+  }
   sendMessage(from, message) {
-    this.messages.push({from:from.email,to:this.email,content:message});
-    sendEmail(from.email,this.email,message)
-  };
-  
+    this.messages.push({ from: from.email, to: this.email, content: message });
+    sendEmail(from.email, this.email, message);
+  }
   showMessagesHistory() {
     if (this.messages.length !== 0) {
       for (const mess of this.messages) {
         console.log(`${mess.from}-> ${mess.to}: ${mess.content}`);
       }
     }
-  };
-};
+  }
+}
 
-let student1 = new User({
-  name: "francisco",
-  surName: "Santos",
-  email: "franciscodos@gmail.com",
-  role: "estudante",
+// let student1 = new User({
+//   name: "francisco",
+//   surName: "Santos",
+//   email: "franciscodos@gmail.com",
+//   role: "estudante",
+// });
+// let student2 = new User({
+//   name: "Kelly",
+//   surName: "Estes",
+//   email: "k_estes@dayrep.com",
+//   role: "student",
+// });
+// let teacher1 = new User({
+//   name: "Paula",
+//   surName: "Thompkins",
+//   email: "PaulaThompkins@jourrapide.com",
+//   role: "teacher",
+// });
+
+// student1.addCourse("maths", 2);
+// student1.addCourse("physics", 1);
+// student1.removeCourse("physics");
+// teacher1.addCourse("biology", 3);
+// teacher1.editCourse("biology", 4);
+// console.log(student1);
+// console.log(teacher1)
+
+// console.log(`${student1.name}: ${student1.courses.length} courses`);
+// console.log(`${teacher1.name}: ${teacher1.courses.length} courses`);
+// teacher1.sendMessage(student1, "ola prof");
+// teacher1.sendMessage(student1, "como estas prof");
+// student1.sendMessage(teacher1,"ola obrigado")
+// student1.sendMessage(teacher1,"estou and you")
+// teacher1.showMessagesHistory();
+// student1.showMessagesHistory();
+// -> franciscodos@gmail.com -> PaulaThompkins@jourrapide.com: ola prof
+// -> franciscodos@gmail.com -> PaulaThompkins@jourrapide.com: como estas prof
+
+// CHALLEGUE#2
+class ExtendedUser extends User {
+  constructor({ name, surname, email, role }) {
+    super({ name, surname, email, role });
+  }
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  }
+  set fullName(fullName) {
+    let names = fullName.split(" ");
+    const [first, ...rest] = names;
+    if (first && rest) {
+      this.name = first;
+      this.surname = rest.join(" ");
+    }
+  }
+
+  static match(teacher, student, course) {
+    let matched = [];
+    for (let scourse of student.courses) {
+      for (let tcourse of teacher.courses) {
+        if (
+          scourse.course === tcourse.course &&
+          scourse.level <= tcourse.level
+        ) {
+          matched.push(scourse);
+        }
+      }
+    }
+    if (course) {
+      for (let mcourse of matched) {
+        if (mcourse.course === course) {
+          return mcourse;
+        }
+      }
+    } else {
+      console.warn(
+        "o professor não ensina os cursos que o estudante está interessado"
+      );
+      return matched;
+    }
+  }
+}
+
+class Teacher extends ExtendedUser {
+  constructor({ name, surname, email }) {
+    super({ name, surname, email, role: "teacher" });
+  }
+}
+
+class Student extends ExtendedUser {
+  constructor({ name, surname, email }) {
+    super({ name, surname, email, role: "student" });
+  }
+}
+
+let student1 = new Student({
+  name: "Francisco",
+  surname: "dosSantos",
+  email: "fraciscodossantos.com",
 });
-let student2 = new User({
+let student2 = new Student({
   name: "Kelly",
-  surName: "Estes",
+  surname: "Estes",
   email: "k_estes@dayrep.com",
-  role: "student",
 });
-let teacher1 = new User({
+let teacher1 = new Teacher({
   name: "Paula",
-  surName: "Thompkins",
+  surname: "Thompkins",
   email: "PaulaThompkins@jourrapide.com",
-  role: "teacher",
 });
 
 student1.addCourse("maths", 2);
-student1.addCourse("physics", 1);
-student1.removeCourse("physics");
-teacher1.addCourse("biology", 3);
-teacher1.editCourse("biology", 4);
-console.log(student1);
-console.log(teacher1)
-
-console.log(`${student1.name}: ${student1.courses.length} courses`);
-console.log(`${teacher1.name}: ${teacher1.courses.length} courses`);
-teacher1.sendMessage(student1, "ola prof");
-teacher1.sendMessage(student1, "como estas prof");
-student1.sendMessage(teacher1,"ola obrigado")
-student1.sendMessage(teacher1,"estou and you")
-teacher1.showMessagesHistory();
-student1.showMessagesHistory();
-// -> franciscodos@gmail.com -> PaulaThompkins@jourrapide.com: test message
-// -> franciscodos@gmail.com -> PaulaThompkins@jourrapide.com: another message
+student1.addCourse("physics", 4);
+teacher1.addCourse("maths", 4);
+let match = ExtendedUser.match(teacher1, student1);
+console.log(match); // -> [{course: 'maths', level: 2}]
+teacher1.editCourse("maths", 1);
+match = ExtendedUser.match(teacher1, student1);
+console.log(match); // -> []
+teacher1.addCourse("physics", 4);
+match = ExtendedUser.match(teacher1, student1, "physics");
+console.log(match); // -> {course: 'physics', level: 4}
+student1.fullName="Francisco Dos Santos";
+console.log(student1.fullName)
