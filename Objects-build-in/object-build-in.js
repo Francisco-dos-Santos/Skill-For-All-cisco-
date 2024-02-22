b9021a485883b21b69e8850c486a021faf9a3b50
Francisco-dos-Santos <franciscodossantosmr@gmail.com>
2024-02-22 10:01:24 -0300

    testing object number, with primitive types number

--- Objects-build-in/object-build-in.js
+++ Objects-build-in/object-build-in.js
@@ -1,6 +1,7 @@
-let numberObj1 = new Number();            // -> 0
-let numberObj2 = new Number(100);       // -> 100
+let numberObj1 = new Number;            // -> 0
+let numberObj2 = Number(100);       // -> 100
 let numberObj3 = new Number("200");     // -> 200
 let numberObj4 = new Number("abcd");    // -> NaN
-let numberObj5 = new Number(9e10000);   // -> Infinity
-console.log(`numberObj1 : ${typeof numberObj1} : ${ numberObj1.valueOf()}`); // -> numberObj1 : object : 0
+let numberObj5 = Number(9e10000);   // -> Infinity
+console.log(`numberObj1 : ${typeof numberObj2} : ${ numberObj2.valueOf()}`); 
+console.log(`numberObj2 : ${typeof numberObj5} : ${ numberObj5.valueOf()}`)
