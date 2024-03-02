6f697821af078163cf6af4d69cf006a47fa6a980
Francisco-dos-Santos <franciscodossantosmr@gmail.com>
2024-03-02 01:30:04 +0100

    add new file the studenting about data

--- Data/data.js
+++ Data/data.js
@@ -9,7 +9,7 @@ console.log(datetwo.toLocaleString()); // -> 01/01/1970, 11:00:00
 console.log(datetwo.toISOString()); // -> 1970-01-01T10:00:00.000Z pega no formato ISO
 
 /*
-date3 = new Date("2020-02-02T20:20:00.000");
+date3 = new Date("2020-02-02T20:20:00.000"); // o ideal para pegar data com string usa form ISO
 date4 = new Date("2020-02-02T20:20:00.000Z");
 console.log(date3.toLocaleString()); // -> 02/02/2020, 20:20:00
 console.log(date3.toUTCString()); // -> Sun, 02 Feb 2020 19:20:00 GMT
@@ -65,3 +65,41 @@ console.log(date10.toLocaleString());
 let dat1 = new Date(2020, 6, 8, 10, 20, 0);
 let dat2 = new Date(2020, 6, 9, 10, 20, 0);
 console.log(dat2.getTime() - dat1.getTime()); // -> 86400000 -> 1000 x 60 x 60 x 24
+let startTime = Date.now();
+for(i=0; i<10000000; i++){}
+let endTime = Date.now();
+console.log(endTime - startTime); //quantos millsegundos seriam apara executar 10milhões interações de for vazio
+
+// operação de componente individual de hora e data
+/*
+getFullYear – obtendo um número que identifique o ano (hora local)
+getUTCFullYear – obtendo um número que identifique o ano (hora UTC+0)
+setFullYear – definindo o valor do ano novo (hora local)
+setUTCFullYear – definindo o valor do ano novo (hora UTC+0)
+
+cada method sempre tera esses quatros componetes (2 get e 2 set)
+
+ano: getFullYear,
+mês: getMonth (lembre-se de que os meses são contados a partir de 0 em JavaScript)
+dia do mês: getDate,
+dia da semana: getDay (os dias da semana são contados a partir de 0 – o primeiro dia é domingo)
+hora: getHours,
+minutos: getMinutes,
+segundos: getSeconds,
+milissegundos: getMilliseconds.
+
+toLocaleDateString - pega a cadeia de catectere da data local  
+toLocaleTimeString. -pega a cadeia de catectere da hora local
+*/
+
+let dates = new Date("2020-07-08T10:20:00"); // ideial para pegar data em string
+console.log(dates.getMonth()); // -> 6 
+console.log(dates.getDay()); // -> 3
+console.log(dates.getDate()); // 8
+console.log(dates.getHours()); // -> 10
+dates.setHours(12);
+console.log(dates.getHours()); // -> 12
+console.log(dates.toLocaleDateString()); // -> 08/07/2020
+console.log(dates.toLocaleTimeString()); // -> 12:20:00
+
+ 
